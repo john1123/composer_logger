63e45feb7f920c14dffb8b9007e851dc067ce4d1
Ivan Kelembetov <kelembetov@gmail.com>
2019-10-13 15:59:57 +0300

    Добавлен вывод backtrace информации

--- src/File.php
+++ src/File.php
@@ -16,13 +16,16 @@ class File {
      * Пишет строку в файл лога.
      * @param $message String Сообщение для запииси
      */
-    public function log($message)
+    public function log($message, $addBacktrace=false)
     {
         $out = '';
         if (strlen($this->dateFormat) > 0) {
             $out = date($this->dateFormat) . ': ';
         }
         $out .= $message . PHP_EOL;
+        if ($addBacktrace == true) {
+            $out .= $this->get_debug_print_backtrace() . PHP_EOL;
+        }
         $this->write($out);
     }
 
@@ -33,5 +36,56 @@ class File {
     public function write($message)
     {
         file_put_contents($this->filename, $message, FILE_APPEND | LOCK_EX);
+    }
+
+    /**
+     *
+     * @see https://www.php.net/manual/ru/function.debug-print-backtrace.php
+     */
+    protected function get_debug_print_backtrace($traces_to_ignore=1)
+    {
+        $traces = debug_backtrace();
+        $ret = array();
+        foreach($traces as $i => $call){
+            if ($i < $traces_to_ignore ) {
+                continue;
+            }
+
+            $object = '';
+            if (isset($call['class'])) {
+                $object = $call['class'].$call['type'];
+                if (is_array($call['args'])) {
+                    foreach ($call['args'] as &$arg) {
+                        $this->get_arg($arg);
+                    }
+                }
+            }
+
+            $ret[] = '#'.str_pad($i - $traces_to_ignore, 3, ' ')
+                .$object.$call['function'].'('.implode(', ', $call['args'])
+                .') called at ['.$call['file'].':'.$call['line'].']';
+        }
+
+        return implode("\n",$ret);
+    }
+
+    /**
+     *
+     * @see https://www.php.net/manual/ru/function.debug-print-backtrace.php
+     */
+    protected function get_arg(&$arg)
+    {
+        if (is_object($arg)) {
+            $arr = (array)$arg;
+            $args = array();
+            foreach($arr as $key => $value) {
+                if (strpos($key, chr(0)) !== false) {
+                    $key = '';    // Private variable found
+                }
+                $args[] =  '['.$key.'] => '.$this->get_arg($value);
+            }
+
+            $arg = get_class($arg) . ' Object ('.implode(',', $args).')';
+        }
     }
 }
